78baf8ad036ac2bc985dab51aadaf2c39c141eab
ASLAM20250827 <aslam20250827@gmail.com>
2025-05-28 19:11:40 +0530

    Add files via upload

---
+++ QQ2.py
@@ -0,0 +1,6 @@
+def BirhdayWish (name,age):
+    print(f"HAPPY BIRTHADAY {name}:{age} ")
+    print("You are now old",name)
+
+BirhdayWish("ASLAM","23")
+# BirhdayWish ("AKTHAR")
---
+++ QQ3.py
@@ -0,0 +1,6 @@
+def Findmaxumnum(num1,num2):
+    if num1<num2:
+        print("max.number is =",num2)
+    else:
+        print("max.number is =",num1)
+Findmaxumnum(10,15)
